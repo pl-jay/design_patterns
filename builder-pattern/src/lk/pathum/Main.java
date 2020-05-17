67a387470dbfae03ac18ec62e72e1e5874ef0959
pl-jay <lakshan.jap@gmail.com>
2020-05-17 13:10:16 +0530

    builder pattern example complete

--- builder-pattern/src/lk/pathum/Main.java
+++ builder-pattern/src/lk/pathum/Main.java
@@ -3,6 +3,37 @@ package lk.pathum;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        /*
+        * Telescoping constructor
+        * */
+        LunchOrderBean lunchOrderBean = new LunchOrderBean();
+
+        lunchOrderBean.setBread("Wheat");
+        lunchOrderBean.setCondiments("lettuce");
+        lunchOrderBean.setMeat("Chicken");
+
+        System.out.println("---Telescoping Constructor--");
+        System.out.println(lunchOrderBean.getBread());
+        System.out.println(lunchOrderBean.getCondiments());
+        System.out.println(lunchOrderBean.getMeat());
+
+        /*
+         * Builder pattern
+         */
+
+        LunchOrder.Builder lunchBuilder = new LunchOrder.Builder();
+
+        lunchBuilder.bread("wheat").condiments("mustard").meat("fish");
+
+        LunchOrder lunchOrder = lunchBuilder.build();
+
+        System.out.println("---Builder Pattern--");
+        System.out.println(lunchOrder.getBread());
+        System.out.println(lunchOrder.getCondiments());
+        System.out.println(lunchOrder.getMeat());
+        System.out.println(lunchOrder.getDressing());
+
+
     }
 }
